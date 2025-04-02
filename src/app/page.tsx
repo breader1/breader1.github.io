ab946f2082cbb7b1d1bea348b5775ff38e7aba75
breader1 <brett.reader@shaw.ca>
2025-04-01 18:01:32 -0700

    removed eslint cause it's annoying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center text-center">
-        <h1 className="text-4xl font-bold">👨‍💻 Brett's Portfolio 🚀</h1>
+        <h1 className="text-4xl font-bold">👨‍💻 Brett&apos;s Portfolio 🚀</h1>
         <h2 className="text-2xl font-bold">🏗️ Under Construction 🛠️</h2>
         <p className="text-lg max-w-[600px]">
           This site is currently being updated. Please check back soon ✨
